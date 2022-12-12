8304630dcf3577fcad6dff853e9343a1009efa0f
Rewale <email@example.com>
2022-12-12 23:12:13 +0500

    parse series

--- services/parser.ts
+++ services/parser.ts
@@ -2,14 +2,11 @@ import parse from "node-html-parser";
 
 
 export default async function ParseSeasons(): Promise<SeasonModel[]> {
-
   const url = "https://sp.freehat.cc/"
   const response = await fetch(url);
   const html = await response.text()
-
   const root = parse(html)
   const seasons_li = root.querySelectorAll("#serial-ep > li")
-
   return seasons_li.map((value, index, array) => {
 
     const num = Number(value.textContent)
@@ -21,5 +18,30 @@ export default async function ParseSeasons(): Promise<SeasonModel[]> {
     }
     return episode
 
+  })
+}
+export async function ParseSeries(seasonUrl: string): Promise<SeriesModel[] | undefined> {
+  const response = await fetch(seasonUrl);
+  const html = await response.text()
+  const root = parse(html)
+  const series_table = root.querySelector(".ep_blocks_table")?.querySelectorAll('.ep_block_center')
+
+  return series_table?.map((value) => {
+    const url = value.getElementsByTagName('a')[0].getAttribute('href')
+    const imageUrl = value.getElementsByTagName('img')[0].getAttribute('src')
+    const name = value.querySelector('.episode-name')?.textContent.trim()
+    const episode_num = Number(value.querySelector('.episode-num')
+      ?.getElementsByTagName('span')[1].textContent)
+    const season_num = Number(value.querySelector('.episode-num')
+      ?.getElementsByTagName('span')[0].textContent)
+
+    const series: SeriesModel = {
+      season: season_num,
+      episode: episode_num,
+      url: url,
+      name: name,
+      preview_url: "https://sp.freehat.cc" + imageUrl
+    }
+    return series
   })
 }
--- tests/parsing.test.ts
+++ tests/parsing.test.ts
@@ -1,4 +1,4 @@
-import ParseSeasons from "../services/parser";
+import ParseSeasons, {ParseSeries} from "../services/parser";
 
 test('parse all seasons', async () => {
   const seasons = await ParseSeasons()
@@ -6,4 +6,15 @@ test('parse all seasons', async () => {
   let firstSeason = seasons.reverse()[1];
   expect(firstSeason.num).toBe(1)
   expect(firstSeason.url).toBe("https://sp.freehat.cc/episode/season-1/")
+});
+test('parse all series', async () => {
+  const url = 'https://sp.freehat.cc/episode/season-1/'
+  const episodes = await ParseSeries(url)
+  expect(episodes?.length).toBe(13)
+
+  const firstEpisode = episodes?.at(0)
+  expect(firstEpisode?.name).toBe("Мамаша Картмана — грязная шлюха")
+  expect(firstEpisode?.episode).toBe(13)
+  expect(firstEpisode?.season).toBe(1)
+  expect(firstEpisode?.preview_url).toBe("https://sp.freehat.cc/upload/iblock/79f/season113_big.jpg")
 });
