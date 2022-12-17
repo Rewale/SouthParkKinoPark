292d8f985d3e596538933981101791a647fe88f0
Rewale <email@example.com>
2022-12-17 16:01:56 +0500

    types

--- src/state/seasonsReducer.ts
+++ src/state/seasonsReducer.ts
@@ -1,31 +1,3 @@
-enum SeasonsActionTypes {
-  FETCH_SEASONS = "FETCH_SEASONS",
-  FETCH_SEASONS_SUCCESS = "FETCH_SEASONS_SUCCESS",
-  FETCH_SEASONS_ERROR = "FETCH_SEASONS_ERROR",
-}
-
-interface SeasonsState {
-  seasons: SeasonModel[],
-  loading: boolean,
-  error: null | string
-}
-
-
-interface FetchSeasonsAction {
-  type: SeasonsActionTypes.FETCH_SEASONS,
-}
-
-interface FetchSeasonsSuccessAction {
-  type: SeasonsActionTypes.FETCH_SEASONS_SUCCESS,
-  payload: SeasonModel[]
-}
-type SeasonsAction = FetchSeasonsAction | FetchSeasonsSuccessAction | FetchSeasonsErrorAction
-
-interface FetchSeasonsErrorAction {
-  type: SeasonsActionTypes.FETCH_SEASONS_ERROR,
-  payload: string,
-}
-
 const initialState: SeasonsState = {
   seasons: [],
   loading: false,
