1fbe88bcf9ef4a7d8f100919876250107fe0a1b8
Rewale <email@example.com>
2022-12-17 18:27:02 +0500

    seasons dropdownpicker

--- src/types/seasons.ts
+++ src/types/seasons.ts
@@ -1,27 +1,27 @@
-enum SeasonsActionTypes {
+export enum SeasonsActionTypes {
   FETCH_SEASONS = "FETCH_SEASONS",
   FETCH_SEASONS_SUCCESS = "FETCH_SEASONS_SUCCESS",
   FETCH_SEASONS_ERROR = "FETCH_SEASONS_ERROR",
 }
 
-interface SeasonsState {
+export interface SeasonsState {
   seasons: SeasonModel[],
   loading: boolean,
   error: null | string
 }
 
 
-interface FetchSeasonsAction {
+export interface FetchSeasonsAction {
   type: SeasonsActionTypes.FETCH_SEASONS,
 }
 
-interface FetchSeasonsSuccessAction {
+export interface FetchSeasonsSuccessAction {
   type: SeasonsActionTypes.FETCH_SEASONS_SUCCESS,
   payload: SeasonModel[]
 }
-type SeasonsAction = FetchSeasonsAction | FetchSeasonsSuccessAction | FetchSeasonsErrorAction
+export type SeasonsAction = FetchSeasonsAction | FetchSeasonsSuccessAction | FetchSeasonsErrorAction
 
-interface FetchSeasonsErrorAction {
+export interface FetchSeasonsErrorAction {
   type: SeasonsActionTypes.FETCH_SEASONS_ERROR,
   payload: string,
 }
